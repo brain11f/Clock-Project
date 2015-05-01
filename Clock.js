6363e1c1ab234c98d577ae2d576b09d321ac242f
Brian Finck <brian@brianfinck.com>
2015-05-01 12:30:21 -0700

    working clock

---
+++ Clock.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    function displayTime() {
+      var currentTime = new Date();
+      var hours = currentTime.getHours();
+      var minutes = currentTime.getMinutes();
+      var seconds = currentTime.getSeconds();
+
+      if (hours < 10) {
+      seconds = '0' + seconds;
+    }
+      if (minutes < 10) {
+      seconds = '0' + seconds;
+    }
+      if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+    var clockDiv = document.getElementById('clock');
+    clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+
+    setInterval(displayTime, 1000);
+
+  }
+
+  displayTime();
+
+});
